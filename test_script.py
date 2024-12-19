1c1ba905d4670f5d03af2d387f0a1c167c4571b5
Leandro Deferrari <deferrari.leandro@gmail.com>
2024-12-19 12:31:01 -0300

    Introduce new intentional error for CI test

--- test_script.py
+++ test_script.py
@@ -1,7 +1,17 @@
+import sys
+
 def add(a, b):
-    return a - b # Probando que tire error
+    return a - b  # Probando que tire error
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+    try:
+        assert add(2, 3) == 5
+        assert add(-1, 1) == 0
+        assert add(0, 0) == 0
+        print("All tests passed!")
+    except AssertionError:
+        print("Test failed!")
+        sys.exit(1)  # Código de error
+
+if __name__ == "__main__":
+    test_add()
